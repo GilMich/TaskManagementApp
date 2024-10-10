d669b1519ed57b33f788d25240f99b12003b857f
GilMich <gilmich@post.bgu.ac.il>
2024-10-10 19:12:04 +0300

    made change that when path "/" is loaded it will automaticalyly navigate to login page.

--- react-frontend/src/components/Layout/Layout.js
+++ react-frontend/src/components/Layout/Layout.js
@@ -1,6 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const Layout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Navigate to Login page by default
+    navigate("/Login");
+  }, []);
   return (
     <div className={styles.pageWrapper}>
         <Outlet />
